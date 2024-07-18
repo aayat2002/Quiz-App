753e6167f65a5e4dc9099e3cbdd3c4c79a8a9303
Sadqua parween <f.naaz.2002a@gmail.com>
2024-07-18 20:33:08 +0530

    changes

---
+++ src/assets/data.js
@@ -0,0 +1,36 @@
+export const data = [
+  {
+    question: "Which language runs in a web browser?",
+    option1: "Java",
+    option2: "C",
+    option3: "Python",
+    option4: "JavaScript",
+    ans: 4,
+  },
+  {
+    question: "Which continent has the highest number of countries?",
+    option1: "Asia",
+    option2: "Europe",
+    option3: "North America",
+    option4: "Africa",
+
+    ans: 4,
+  },
+  {
+    question: "What does HTML stand for?",
+    option1: "Hypertext Markup Language",
+    option2: "Hypertext Markdown Language",
+    option3: "Hyperloop Machine Language",
+    option4: "Helicopters Terminals Motorboats Lamborginis",
+
+    ans: 1,
+  },
+  {
+    question: "What year was JavaScript launched?",
+    option1: "1996",
+    option2: "1995",
+    option3: "1994",
+    option4: "none of the above",
+    ans: 2,
+  },
+];
